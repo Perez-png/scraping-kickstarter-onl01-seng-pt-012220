f5df482c39a68fcc057d13de72f04063222e9fbf
Xavier Perez <xperez007@gmail.com>
2020-03-01 16:02:53 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,16 +13,16 @@ Nokogiri::HTML(html)
   binding.pry
 end
 
-# projects: kickstarter.css("li.project.grid_4")
-# title: project.css("h2.bbcard_name strong a").text
-# image link: project.css("div.project-thumbnail a img").attribute("src").value
-# description: project.css("p.bbcard_blurb").text
-# location: project.css("ul.project-meta span.location-name").text
-# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-
-end
-
-
+ projects: kickstarter.css("li.project.grid_4")
+ title: project.css("h2.bbcard_name strong a").text
+ image link: project.css("div.project-thumbnail a img").attribute("src").value
+ description: project.css("p.bbcard_blurb").text
+ location: project.css("ul.project-meta span.location-name").text
+ percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+ 
+ end
+ 
+ 
 end
 
 find_my_way_project
